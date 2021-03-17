481cf8a268f7397eb819d426383b394d959a50f3
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-03-16 21:55:22 -0300

    Habilitado cache

--- src/main/java/br/com/renanrramos/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/renanrramos/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.renanrramos.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,8 @@ import br.com.renanrramos.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, String> {
 
+
+	@Cacheable("by_status")
 	List<Pedido> findByStatus(StatusPedido aguardando, Pageable sort);
 
 	@Query("select p from Pedido p join p.user u where u.username = :username")
